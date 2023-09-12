34d7109c5e10ba2df720b89039bfbd1fa8d02759
sanidhya jain <sanidhyajain21@gmail.com>
2023-09-12 23:53:33 +0530

    auto fill implementation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect } from 'react'
 
 function App() {
+
+  const [otp,setOtp] = useState('')
+  useEffect(()=>{
+    if ('OTPCredential' in window) { 
+      console.log('event is there')
+    }
+  },[])
+
+  useEffect(()=>{
+    const ac = new AbortController();
+    const otpFill=()=>{
+      navigator.credentials.get({
+        otp: { transport:['sms'] },
+        signal: ac.signal
+      }).then(otp => {
+        alert(otp.code)
+        setOtp(otp.code)
+      }).catch(err => {
+        console.log(err)
+      });
+    }
+
+      otpFill()
+  })
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form action="/verify-otp" method="POST">
+          <label>Auto Fill otp</label>
+          <input type="text"
+            value={otp}
+            onChange={(e)=>setOtp(e.target.value)}
+            inputMode="numeric"
+            autoComplete="one-time-code"
+            pattern="\d{6}"
+            
+            required />
+        </form>
+
       </header>
     </div>
   );
